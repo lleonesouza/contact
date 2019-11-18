1ceb6b12a9f978f5ed675006ef940ae8e2080bd0
lleonesouza <aliencode.host@gmail.com>
2019-11-18 19:54:25 -0300

    Updates

---
+++ precache-manifest.d6c699e9b26f87291a67435b75cd7957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8225cd9892d73eb82b32845de3b8ae94",
+    "url": "/contact/index.html"
+  },
+  {
+    "revision": "fd3144d9d730add7bd24",
+    "url": "/contact/static/css/main.1512fa5c.chunk.css"
+  },
+  {
+    "revision": "41028ac793f2464c4092",
+    "url": "/contact/static/js/2.75e67943.chunk.js"
+  },
+  {
+    "revision": "fd3144d9d730add7bd24",
+    "url": "/contact/static/js/main.7595f328.chunk.js"
+  },
+  {
+    "revision": "08ab9e0457d8a9e12251",
+    "url": "/contact/static/js/runtime-main.2423729b.js"
+  },
+  {
+    "revision": "b1dae43e4eded148b52825a4ee5a80ee",
+    "url": "/contact/static/media/perfil.b1dae43e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ffmagico/precache-manifest.951a8970180b0176c711bb73d869bbfa.js"
+  "/contact/precache-manifest.d6c699e9b26f87291a67435b75cd7957.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ffmagico/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/contact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
